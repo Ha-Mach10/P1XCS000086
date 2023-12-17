fc0fd589cd6b680e764f9623ff361d7ddbc79440
Ha-Mach10 <momiji0846to1026@outlook.com>
2023-12-18 07:18:38 +0900

    下記更新。 2023/12/18

---
+++ P1XCS000086/Services/P1XCS000086.Services/IO/JsonExtention.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+using Newtonsoft.Json;
+
+namespace P1XCS000086.Services.IO
+{
+	public static class JsonExtention
+	{
+		public static async void SerializeJson<T>(T obj, string path, bool append)
+		{
+			try
+			{
+				using (var sw = new StreamWriter(path, append, Encoding.UTF8))
+				{
+					var jsonData = JsonConvert.SerializeObject(obj, Formatting.Indented);
+					await sw.WriteAsync(jsonData);
+				}
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine(ex);
+			}
+		}
+
+		public static T DeserializeJson<T>(string path)
+		{
+			try
+			{
+				// StreamReaderでファイルを読み込む
+				using (var sr = new StreamReader(path, Encoding.UTF8))
+				{
+					// 
+					var jsonData = sr.ReadToEnd();
+					// 
+					if (jsonData == string.Empty) { return default; }
+					return JsonConvert.DeserializeObject<T>(jsonData);
+				}
+			}
+			catch(Exception ex)
+			{
+				Debug.WriteLine(ex);
+				return default;
+			}
+		}
+	}
+}
---
+++ P1XCS000086/Services/P1XCS000086.Services/Objects/JsonConnectionStrings.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text;
+
+namespace P1XCS000086.Services.Objects
+{
+	[JsonObject("JsonConnectionStrings")]
+	internal class JsonConnectionStrings
+	{
+		[JsonProperty("Server")]
+		public string Server {  get; set; }
+
+		[JsonProperty("User")]
+		public string User {  get; set; }
+
+		[JsonProperty("DatabaseName")]
+		public string DatabaseName {  get; set; }
+
+		[JsonProperty("Password")]
+		public string Password { get; set; }
+
+		[JsonProperty("PersistSecurityInfo")]
+		public bool PersistSecurityInfo { get; set; }
+	}
+}
