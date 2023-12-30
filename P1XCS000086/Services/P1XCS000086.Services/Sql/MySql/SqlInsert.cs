ef8ca906d02ed5bdfcfeb3cdb2062244aee58f76
Ha-Mach10 <momiji0846to1026@outlook.com>
2023-12-30 13:05:46 +0900

    下記更新。 2023/12/30 13:05

---
+++ P1XCS000086/Services/P1XCS000086.Services/Sql/MySql/SqlInsert.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace P1XCS000086.Services.Sql.MySql
+{
+	public class SqlInsert
+	{
+		private string _connStr;
+		private string _command;
+
+
+		public string ResultMessage { get; private set; }
+		public string ExceptionMessage {  get; private set; }
+
+		
+		public SqlInsert(string connStr)
+		{
+			_connStr = connStr;
+		}
+		public SqlInsert(string connStr, string command) : this(connStr)
+		{
+			_command = command;
+		}
+
+
+		public void Insert(string command, List<string> columns, List<string> values)
+		{
+			try
+			{
+				// コネクションを生成し、コマンドを生成
+				using (MySqlConnection conn = new MySqlConnection(_connStr))
+				using (MySqlCommand cmd = new MySqlCommand(command, conn))
+				{
+					MySqlTransaction tran = null;
+					
+					try
+					{
+						// コネクションを開く
+						conn.Open();
+						// トランザクションを開始
+						tran = conn.BeginTransaction();
+
+						// コマンドパラメータを設定（SQLインジェクション対策）
+						for (int i = 0; i > columns.Count; i++)
+						{
+							cmd.Parameters.Add(new MySqlParameter(columns[i], values[i]));
+						}
+
+						// コマンドを実行
+						var result = cmd.ExecuteNonQuery();
+
+						// 実行された結果が1行未満のとき
+						if (result != 1)
+						{
+							ResultMessage = "データを更新できませんでした。";
+
+							// ロールバックする
+							tran.Rollback();
+							return;
+						}
+
+						// コミットする
+						tran.Commit();
+					}
+					// データベース操作で例外が発生した場合
+					catch (MySqlException ex)
+					{
+						ExceptionMessage = $"Error: {ex.Message}";
+					}
+				}
+			}
+			// コネクションおよびコマンド生成時に例外が発生した場合
+			catch(MySqlException ex)
+			{
+				ExceptionMessage = ex.Message;
+			}
+		}
+	}
+}
