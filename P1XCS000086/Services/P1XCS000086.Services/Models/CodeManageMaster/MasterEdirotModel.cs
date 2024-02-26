35894bbaace5c5feb895db8f0baa83e7e061f0cf
知らん仮名 <momiji0846to1026@outlook.com>
2024-02-27 07:18:43 +0900

    ロジック追加。 2024/02/27 07:18

---
+++ P1XCS000086/Services/P1XCS000086.Services/Models/CodeManageMaster/MasterEdirotModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using P1XCS000086.Services.Interfaces.Models.CodeManageMaster;
+
+namespace P1XCS000086.Services.Models.CodeManageMaster
+{
+	public class MasterEdirotModel : IMasterEdirotModel
+	{
+
+	}
+}
