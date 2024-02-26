7e40d73741ea9942aadd9be01c55a83280382c6f
知らん仮名 <momiji0846to1026@outlook.com>
2024-02-26 21:34:55 +0900

    下記更新。 2024/02/26 21:34

--- P1XCS000086/Modules/P1XCS000086.Modules.CodeManageMaster/ViewModels/MasterEditorViewModel.cs
+++ P1XCS000086/Modules/P1XCS000086.Modules.CodeManageMaster/ViewModels/MasterEditorViewModel.cs
@@ -1,14 +1,21 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
-using System;
+﻿using System;
+using System.Reactive.Disposables;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Prism.Regions;
+using Prism.Commands;
+using Prism.Mvvm;
+
+using Reactive.Bindings.Extensions;
+using Prism.Navigation;
+
+
 namespace P1XCS000086.Modules.CodeManageMaster.ViewModels
 {
-	public class MasterEditorViewModel : BindableBase
+	public class MasterEditorViewModel : BindableBase, IDestructible
 	{
 		private string _message;
 		public string Message
@@ -17,9 +24,32 @@ namespace P1XCS000086.Modules.CodeManageMaster.ViewModels
 			set { SetProperty(ref _message, value); }
 		}
 
+		// ****************************************************************************
+		// Fields
+		// ****************************************************************************
+
+		private CompositeDisposable _disposable;
+
+
+
+		// ****************************************************************************
+		// Constructor
+		// ****************************************************************************
+
 		public MasterEditorViewModel()
 		{
 			Message = "View A from your Prism Module";
+		}
+
+
+
+		// ****************************************************************************
+		// Public Methods
+		// ****************************************************************************
+
+		public void Destroy()
+		{
+
 		}
 	}
 }
