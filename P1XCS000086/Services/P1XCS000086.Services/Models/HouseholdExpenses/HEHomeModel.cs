1623f74f4dfb5fd1d058633865afebc69a673650
知らん仮名 <momiji0846to1026@outlook.com>
2024-08-17 22:52:08 +0900

    下記更新。

---
+++ P1XCS000086/Services/P1XCS000086.Services/Models/HouseholdExpenses/HEHomeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P1XCS000086.Services.Models.HouseholdExpenses
+{
+	public class HEHomeModel
+	{
+		public HEHomeModel()
+		{
+
+		}
+	}
+}
